09610938ee1d4fe6d536ea1caffa712a258ae13b
GaryMcCully-UWGB <66743028+GaryMcCully-UWGB@users.noreply.github.com>
2020-07-07 07:43:00 -0500

    Create Project_3_MontyHallProblem.java

---
+++ Project_3_MontyHallProblem.java
@@ -0,0 +1,25 @@
+//This code simulates the Monty Hall Problem by simulating the Let's Make a Deal gameshow
+//More information regarding the problem can be found at the following link: https://en.wikipedia.org/wiki/Monty_Hall_problem
+
+import java.util.*;
+import java.text.*;
+
+public class Project_3_MontyHallProblem
+{
+  public static void main(String[] args)
+
+  {
+    Random randNumObj = new Random();
+    int winningDoor = randNumObj.nextInt(2);
+    int notDoor = randNumObj.nextInt(1);
+    int totalGames = 0;
+    int gamesWon = 0;
+    int gamesLost = 0;
+
+//    String[] orderInfo=new String[7];
+//    Scanner user_input= new Scanner(System.in);
+
+    System.out.println("Welcome to Lets Make a Deal!\n");
+    
+  }
+}
